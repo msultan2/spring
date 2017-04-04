514914c39f99b65cf0213a77ccd7423c21c335de
Mohamed Sultan <mohamed.sultan1581@gmail.com>
2017-04-04 21:20:32 +0100

    Dependency Injection by type (different implementations)

--- dependencyinjectionexample/src/main/java/com/mycompany/config/HelloConfig.java
+++ dependencyinjectionexample/src/main/java/com/mycompany/config/HelloConfig.java
@@ -4,6 +4,7 @@ import com.mycompany.services.HelloWorldFactory;
 import com.mycompany.services.HelloWorldService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 
 /**
@@ -19,13 +20,35 @@ public class HelloConfig {
 
     @Bean
     @Profile("english")
+    @Primary //Used to direct spring to select this one other than the other 5 (in case of reference by type)
     public HelloWorldService helloWorldServiceEnglish(HelloWorldFactory factory){
         return factory.createHelloWorldService("en");
     }
 
     @Bean
     @Profile("spanish")
+    @Primary    //Used to direct spring to select this one other than the other 5 (in case of reference by type)
     public HelloWorldService helloWorldServiceSpanish(HelloWorldFactory factory){
         return factory.createHelloWorldService("es");
+    }
+
+    @Bean(name = "french")
+    public HelloWorldService helloWorldServiceFrench(HelloWorldFactory factory){
+        return factory.createHelloWorldService("fr");
+    }
+
+    @Bean
+    public HelloWorldService helloWorldServiceGermanX(HelloWorldFactory factory){
+        return factory.createHelloWorldService("de");
+    }
+
+    @Bean
+    public HelloWorldService helloWorldServicePolish(HelloWorldFactory factory){
+        return factory.createHelloWorldService("pl");
+    }
+
+    @Bean
+    public HelloWorldService helloWorldServiceRussian(HelloWorldFactory factory){
+        return factory.createHelloWorldService("ru");
     }
 }
