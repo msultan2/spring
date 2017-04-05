b0f2fc11f1cc9c4cbadfc8d1d16c427eb6241589
Mohamed Sultan <mohamed.sultan1581@gmail.com>
2017-04-05 09:59:32 +0100

    Using xml instead of annotations.

--- dependencyinjectionexample/src/main/java/springboot/DependencyinjectionexampleApplication.java
+++ dependencyinjectionexample/src/main/java/springboot/DependencyinjectionexampleApplication.java
@@ -6,9 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ComponentScan("com.mycompany")
+//@ComponentScan("com.mycompany")
+@ImportResource("classpath:/spring/spring-config.xml")
 public class DependencyinjectionexampleApplication {
 
 	public static void main(String[] args) {
