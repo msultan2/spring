489f19a88e88c685318b595dd9eacf3021136d5e
Mohamed Sultan <mohamed.sultan1581@gmail.com>
2017-04-05 15:33:06 +0100

    Create a basic list of products complete MVC

---
+++ springmvc/src/main/java/com/mycompany/controllers/ProductController.java
@@ -0,0 +1,28 @@
+package com.mycompany.controllers;
+
+import com.mycompany.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by mohamedsultan on 05/04/2017.
+ */
+
+@Controller
+public class ProductController {
+
+    private ProductService productService;
+
+    @Autowired
+    public void setProductService(ProductService productService){
+        this.productService = productService;
+    }
+
+    @RequestMapping("/products")
+    public String listProducts(Model model){
+        model.addAttribute("products", productService.listAllProducts());
+        return "products";
+    }
+}
