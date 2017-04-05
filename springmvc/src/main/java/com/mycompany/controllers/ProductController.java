74ac8176dabcb2860f9d6d918c57cbef5854aefb
Mohamed Sultan <mohamed.sultan1581@gmail.com>
2017-04-05 16:03:25 +0100

    Added new one product

--- springmvc/src/main/java/com/mycompany/controllers/ProductController.java
+++ springmvc/src/main/java/com/mycompany/controllers/ProductController.java
@@ -1,10 +1,14 @@
 package com.mycompany.controllers;
 
+import com.mycompany.domain.Product;
 import com.mycompany.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.websocket.server.PathParam;
 
 /**
  * Created by mohamedsultan on 05/04/2017.
@@ -24,5 +28,12 @@ public class ProductController {
     public String listProducts(Model model){
         model.addAttribute("products", productService.listAllProducts());
         return "products";
+    }
+
+    @RequestMapping("product/{id}")
+    public String getProduct(@PathVariable Integer id, Model model){
+        model.addAttribute("product",productService.getProductById(id));
+
+        return "product";
     }
 }
